e59f19eca16af5742c0b19ea42650fe1f0d2c84c
chinmai <me@chinmai.dev>
2020-12-13 14:04:46 -0800

    mobile works better now (i think)

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 
 // Costs of items, dollars
-var itemCosts = {"pizza":20, "toyota corolla": 19925, "iPhone 12 Pro": 999, "Airpods Max":549, "Airpods":119, "One gallon of gas": 4, "Double Double Burger Combo":7.84, "Your College Tuition (in state)":13991, "Your College Tuition (out of state)":43745, "One month of netfix":8.99 };
+var itemCosts = { "pizza": 20, "toyota corolla": 19925, "iPhone 12 Pro": 999, "Airpods Max": 549, "Airpods": 119, "One gallon of gas": 4, "Double Double Burger Combo": 7.84, "Your College Tuition (in state)": 13991, "Your College Tuition (out of state)": 43745, "One month of netfix": 8.99 };
 
 // Salary of people, dollars per hour
-var peopleSalaries = {"me":12, "Jeff":13400000};
+var peopleSalaries = { "Chinmai": 12, "Jeff": 13400000 };
 // Jeff https://marketrealist.com/p/how-much-does-jeff-bezos-make-a-second/ 
 
 
-function getRealTime(income, item){
+function getRealTime(income, item) {
     // returns time in milliseconds (i think)
-    return itemCosts[item] / peopleSalaries[income] * 60 *60 *1000;
+    return itemCosts[item] / peopleSalaries[income] * 60 * 60 * 1000;
 }
 var startertime
 var totalTime
-function holder(){
+function holder() {
     startertime = Date.now();
 
 }
-function letGo(){
-    totalTime =  Date.now() - startertime;
+function letGo() {
+    totalTime = Date.now() - startertime;
     console.log(totalTime)
     updateScreen(totalTime, getPerson(), getItem())
 }
-function updateScreen(time, salary, item){
+function updateScreen(time, salary, item) {
     // shows the time difference somehow
     td = getRealTime(salary, item) - time;
     console.log(getRealTime(salary, item))
-    document.getElementById("out").innerHTML = "You are off by" + (td/1000) + " seconds. try again if you would like"
+    document.getElementById("out").innerHTML = "You are off by" + (td / 1000) + " seconds. try again if you would like"
 
 }
 
-function createDropElement(name){
+function createDropElement(name) {
     var opt = document.createElement("option")
-    opt.id=name;
+    opt.id = name;
     opt.value = name;
     opt.innerHTML = name;
     return opt;
@@ -40,9 +40,9 @@ function createDropElement(name){
 }
 
 // creates a dropdown that pulls items from the dict itemCosts
-function createItemDrop(){
+function createItemDrop() {
     selectHTML = document.getElementById("itdrop")
-    for (person in itemCosts){
+    for (person in itemCosts) {
         console.log(person)
         selectHTML.appendChild(createDropElement(person))
     }
@@ -50,23 +50,67 @@ function createItemDrop(){
 
 
 // Creates a menu that pulls items from the dict peopleSalaries
-function createPeopleDrop(){
+function createPeopleDrop() {
     selectHTML = document.getElementById("indrop")
-    for (person in peopleSalaries){
+    for (person in peopleSalaries) {
         console.log(person)
         selectHTML.appendChild(createDropElement(person))
     }
 }
 
 // Gets the currently selected salary in js
-function getPerson(){
+function getPerson() {
     return document.getElementById("indrop").value
 }
 
 // Gets the currently selected item in js
-function getItem(){
+function getItem() {
     return document.getElementById("itdrop").value
 }
 
 createPeopleDrop();
 createItemDrop();
+
+
+
+function absorbEvent_(event) {
+    var e = event || window.event;
+    e.preventDefault && e.preventDefault();
+    e.stopPropagation && e.stopPropagation();
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+}
+function absorbTouchStart_(event){
+        holder();
+        var e = event || window.event;
+    e.preventDefault && e.preventDefault();
+    e.stopPropagation && e.stopPropagation();
+    e.cancelBubble = true;
+    e.returnValue = false;
+
+    return false;
+}
+function absorbTouchEnd_(event){
+    console.log("hmm")
+        letGo();
+        var e = event || window.event;
+    e.preventDefault && e.preventDefault();
+    e.stopPropagation && e.stopPropagation();
+    e.cancelBubble = true;
+    e.returnValue = false;
+
+    return false;
+}
+
+function preventLongPressMenu(node) {
+    node.ontouchstart = absorbTouchStart_;
+    node.ontouchmove = absorbEvent_;
+    node.ontouchend = absorbTouchEnd_;
+    node.ontouchcancel = absorbEvent_;
+}
+
+function init() {
+    preventLongPressMenu(document.getElementById('holdbutton'));
+}
+init()
