0ae6cecac133ebe24a9d8fe0942596703fca97da
chinmai <me@chinmai.dev>
2020-12-13 12:44:11 -0800

    js template is done for the most part

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,48 @@
-var itemCosts = {};
-var peopleSalaries = {};
+
+// Costs of items, dollars
+var itemCosts = {"pizza":20, "toyota corolla": 19925};
+
+// Salary of people, dollars per hour
+var peopleSalaries = {"me":12};
 
 
+
+function getRealTime(income, item){
+    // returns time in milliseconds (i think)
+    return itemCosts[item] / peopleSalaries[income] * 60 *60 *1000;
+}
+var startertime
+var totalTime
+function holder(){
+    startertime = Date.now();
+
+}
+function letGo(){
+    totalTime =  Date.now() - startertime;
+    console.log(totalTime)
+    updateScreen(totalTime)
+}
+function updateScreen(time, salary, item){
+    // shows the time difference somehow
+    console.log(getRealTime(salary, item) - time);
+}
+
+// creates a dropdown that pulls items from the dict itemCosts
+function createItemDrop(){
+
+}
+
+// Creates a menu that pulls items from the dict peopleSalaries
+function createPeopleDrop(){
+
+}
+
+// Gets the currently selected salary in js
+function getPeople(){
+
+}
+
+// Gets the currently selected item in js
+function getItem(){
+
+}
